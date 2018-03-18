1f9d16747bd15b1ed28d59b3f20d97ba43e170c3
Oleksandr Hryb <oleksandr.hryb@outlook.com>
2018-03-18 19:52:12 +0200

    Гортаємо сторінки, перебираємо список лотів

--- STEAM_Crawler/Form1.Designer.cs
+++ STEAM_Crawler/Form1.Designer.cs
@@ -28,12 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbSteamInitialLink = new System.Windows.Forms.TextBox();
+            this.btnStartBrowser = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblPagesCount = new System.Windows.Forms.Label();
+            this.GotoNextPage = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnReadList = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbSteamInitialLink
+            // 
+            this.tbSteamInitialLink.Location = new System.Drawing.Point(30, 22);
+            this.tbSteamInitialLink.Name = "tbSteamInitialLink";
+            this.tbSteamInitialLink.Size = new System.Drawing.Size(533, 20);
+            this.tbSteamInitialLink.TabIndex = 0;
+            this.tbSteamInitialLink.Text = resources.GetString("tbSteamInitialLink.Text");
+            // 
+            // btnStartBrowser
+            // 
+            this.btnStartBrowser.Location = new System.Drawing.Point(488, 48);
+            this.btnStartBrowser.Name = "btnStartBrowser";
+            this.btnStartBrowser.Size = new System.Drawing.Size(75, 23);
+            this.btnStartBrowser.TabIndex = 1;
+            this.btnStartBrowser.Text = "Open Browser";
+            this.btnStartBrowser.UseVisualStyleBackColor = true;
+            this.btnStartBrowser.Click += new System.EventHandler(this.btnGetStat_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblPagesCount
+            // 
+            this.lblPagesCount.AutoSize = true;
+            this.lblPagesCount.Location = new System.Drawing.Point(35, 55);
+            this.lblPagesCount.Name = "lblPagesCount";
+            this.lblPagesCount.Size = new System.Drawing.Size(35, 13);
+            this.lblPagesCount.TabIndex = 3;
+            this.lblPagesCount.Text = "label1";
+            // 
+            // GotoNextPage
+            // 
+            this.GotoNextPage.Location = new System.Drawing.Point(488, 77);
+            this.GotoNextPage.Name = "GotoNextPage";
+            this.GotoNextPage.Size = new System.Drawing.Size(75, 23);
+            this.GotoNextPage.TabIndex = 4;
+            this.GotoNextPage.Text = "button2";
+            this.GotoNextPage.UseVisualStyleBackColor = true;
+            this.GotoNextPage.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 106);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(572, 194);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // btnReadList
+            // 
+            this.btnReadList.Location = new System.Drawing.Point(298, 67);
+            this.btnReadList.Name = "btnReadList";
+            this.btnReadList.Size = new System.Drawing.Size(75, 23);
+            this.btnReadList.TabIndex = 6;
+            this.btnReadList.Text = "button3";
+            this.btnReadList.UseVisualStyleBackColor = true;
+            this.btnReadList.Click += new System.EventHandler(this.btnReadList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(596, 336);
+            this.Controls.Add(this.btnReadList);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.GotoNextPage);
+            this.Controls.Add(this.lblPagesCount);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnStartBrowser);
+            this.Controls.Add(this.tbSteamInitialLink);
+            this.Name = "Form1";
+            this.Text = "STEAM CRAWLER";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbSteamInitialLink;
+        private System.Windows.Forms.Button btnStartBrowser;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblPagesCount;
+        private System.Windows.Forms.Button GotoNextPage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnReadList;
     }
 }
 
--- STEAM_Crawler/Form1.cs
+++ STEAM_Crawler/Form1.cs
@@ -4,17 +4,72 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
 
 namespace STEAM_Crawler
 {
     public partial class Form1 : Form
     {
+        IWebDriver browser;
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.Columns.Add( "itemName","ITEM_NAME");
+            dataGridView1.Columns.Add("itemLink", "ITEM_LINK");
+        }
+
+        private void btnGetStat_Click(object sender, EventArgs e)
+        {
+            browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            browser.Navigate().GoToUrl(this.tbSteamInitialLink.Text);
+
+            List<IWebElement> Pages = browser.FindElements(By.ClassName("market_paging_pagelink")).ToList();
+            lblPagesCount.Text = Pages.Last().Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            browser.Quit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            browser.FindElement(By.Id("searchResults_btn_next")).Click();
+            //searchResults_btn_next
+        }
+
+        private void btnReadList_Click(object sender, EventArgs e)
+        {
+            //market_listing_row market_recent_listing_row market_listing_searchresult
+
+            //market_listing_row_link
+            List<IWebElement> results = browser.FindElements(By.ClassName("market_listing_row_link")).ToList();
+            foreach (IWebElement result in results)
+            {
+                var item = result.FindElement(By.ClassName("market_listing_searchresult"));
+                var itemName = item.FindElement(By.ClassName("market_listing_item_name_block"));
+                var sName = itemName.FindElement(By.ClassName("market_listing_item_name"));
+                this.dataGridView1.Rows.Add(sName.Text, result.GetAttribute("href"));
+
+                using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+                {
+                 
+                    // Or you can get the file content without saving it:
+                    string htmlCode = client.DownloadString(result.GetAttribute("href"));
+                    string strStartCriteria = "var line1=[";
+                    string strEndCriteria = "g_timePriceHistoryEarliest";
+                    int startPosition = htmlCode.IndexOf(strStartCriteria);
+                    int endPosition = htmlCode.IndexOf(strEndCriteria, startPosition);
+                    int substrLength = endPosition - (strStartCriteria.Length + startPosition);
+                    string stat = htmlCode.Substring((startPosition + strStartCriteria.Length), substrLength);
+                    string[] cells = stat.Split(new string[] { "],[" }, StringSplitOptions.None);
+                    //...
+                }
+            }
         }
     }
 }
